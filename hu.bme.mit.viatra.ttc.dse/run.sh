153f10c1f268a836ef6e130f852c3058dd0833ba
Gabor Szarnyas <szarnyasg@gmail.com>
2016-04-26 20:32:11 +0200

    Added run script, fixed missing dependency

--- hu.bme.mit.viatra.ttc.dse/run.sh
+++
@@ -1,1 +0,0 @@
-java -cp "target/hu.bme.mit.viatra.ttc.dse-0.0.1-SNAPSHOT.jar:lib/*" hu.bme.mit.viatra.ttc.dse.DseSolverMain
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "$0" )" && pwd )"
+cd hu.bme.mit.viatra.ttc.dse
+java -cp "target/hu.bme.mit.viatra.ttc.dse-0.0.1-SNAPSHOT.jar:target/lib/*" hu.bme.mit.viatra.ttc.dse.DseSolverMain
