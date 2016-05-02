0998853d7a41ef6a38e0d20009e9fc993f953a63
András Szabolcs Nagy <nasz013@gmail.com>
2016-05-02 13:49:56 +0200

    A simple test problem with four features and DFS

---
+++ hu.bme.mit.viatra.ttc.dse/src/hu/bme/mit/viatra/ttc/dse/SimpleCraDseRunner.java
@@ -0,0 +1,82 @@
+package hu.bme.mit.viatra.ttc.dse;
+
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.eclipse.viatra.dse.api.DesignSpaceExplorer;
+import org.eclipse.viatra.dse.api.Strategies;
+import org.eclipse.viatra.dse.base.DesignSpaceManager;
+import org.eclipse.viatra.dse.objectives.Comparators;
+import org.eclipse.viatra.dse.objectives.impl.ConstraintsObjective;
+import org.eclipse.viatra.dse.solutionstore.SolutionStore;
+import org.eclipse.viatra.query.runtime.exception.ViatraQueryException;
+import org.junit.Test;
+
+import architectureCRA.ArchitectureCRAFactory;
+import architectureCRA.ArchitectureCRAPackage;
+import architectureCRA.Attribute;
+import architectureCRA.ClassModel;
+import architectureCRA.Method;
+import hu.bme.mit.viatra.ttc.dse.objectives.CraIndexObjective;
+import hu.bme.mit.viatra.ttc.dse.queries.util.AllFeatureEncapsulatedQuerySpecification;
+import hu.bme.mit.viatra.ttc.dse.queries.util.NoEmptyClassQuerySpecification;
+import hu.bme.mit.viatra.ttc.dse.queries.util.NotEncapsulatedFeatureQuerySpecification;
+import hu.bme.mit.viatra.ttc.dse.rules.CraDseRules;
+import hu.bme.mit.viatra.ttc.dse.statecoder.CraStateCoderFactory;
+
+public class SimpleCraDseRunner {
+
+    @Test
+    public void runDseWithInputModel() throws IOException, ViatraQueryException {
+        
+        Logger.getLogger(DesignSpaceManager.class).setLevel(Level.DEBUG);
+        
+        ArchitectureCRAFactory factory = ArchitectureCRAFactory.eINSTANCE;
+        ClassModel model = factory.createClassModel();
+        Method m1 = factory.createMethod();
+        Method m2 = factory.createMethod();
+        Method m3 = factory.createMethod();
+        Attribute a1 = factory.createAttribute();
+        m1.setName("M1");
+        m2.setName("M2");
+        m3.setName("M3");
+        a1.setName("A1");
+        model.getFeatures().add(m1);
+        model.getFeatures().add(m2);
+        model.getFeatures().add(m3);
+        model.getFeatures().add(a1);
+        m1.getFunctionalDependency().add(m2);
+        m2.getFunctionalDependency().add(m3);
+        m2.getDataDependency().add(a1);
+        
+        DesignSpaceExplorer dse = new DesignSpaceExplorer();
+        
+        dse.setInitialModel(model);
+        dse.addMetaModelPackage(ArchitectureCRAPackage.eINSTANCE);
+        dse.setStateCoderFactory(new CraStateCoderFactory());
+        
+        CraDseRules rules = new CraDseRules();
+        dse.addTransformationRule(rules.createClassRule);
+        dse.addTransformationRule(rules.addFeatureRule);
+        
+        dse.addObjective(new ConstraintsObjective()
+                .withHardConstraint("allFeatureEncapsulated", AllFeatureEncapsulatedQuerySpecification.instance())
+                .withHardConstraint("noEmtpyClass", NoEmptyClassQuerySpecification.instance())
+                .withSoftConstraint("unusedFeature", NotEncapsulatedFeatureQuerySpecification.instance(), 1)
+                .withComparator(Comparators.LOWER_IS_BETTER)
+                .withLevel(0)
+                );
+        dse.addObjective(new CraIndexObjective().withLevel(0));
+        
+        SolutionStore solutionStore = new SolutionStore(0);
+        solutionStore.logSolutionsWhenFound();
+        dse.setSolutionStore(solutionStore);
+        
+        dse.startExploration(Strategies.createDFSStrategy(0));
+        
+        System.out.println(dse.toStringSolutions());
+        
+    }
+
+}
