3ee01338e1b35c16537e88a4d1a66acbcf8a2edf
Gabor Szarnyas <szarnyasg@gmail.com>
2016-05-07 19:23:04 +0200

    Results in gitignore

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )"
 cd hu.bme.mit.viatra.ttc.dse
-java -Dorg.eclipse.emf.common.util.ReferenceClearingQueue=false -cp "target/hu.bme.mit.viatra.ttc.dse-0.0.1-SNAPSHOT.jar:target/lib/*" hu.bme.mit.viatra.ttc.dse.DseSolverMain $@
+
+java -Dorg.eclipse.emf.common.util.ReferenceClearingQueue=false -cp "target/hu.bme.mit.viatra.ttc.dse-0.0.1-SNAPSHOT.jar:target/lib/*" hu.bme.mit.viatra.ttc.dse.DseSolverMain $@ | tee results.log
